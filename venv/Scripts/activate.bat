7efce368428b245104d446e46bddf251e902b925
Savindu <savinduruhunuhewa@gmail.com>
2025-01-31 09:30:50 +0530

    Install the requirment file and setup enviornment

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\THIS PC\Desktop\Financial Agent\venv
+set VIRTUAL_ENV=C:\Users\THIS PC\Desktop\Financial Agent\Financial-Agent-for-NVIDIA-stock-analysis\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
