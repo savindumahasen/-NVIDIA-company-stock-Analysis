2a0647f3ba66e860e5b4fd29f216df27a42a2ce4
Savindu Mahasen Ruhunuhewa <88643915+savindumahasen@users.noreply.github.com>
2025-02-01 10:11:02 +0530

    Update financial_agent.py

--- financial_agent.py
+++ financial_agent.py
@@ -1,28 +1,44 @@
 from phi.agent import Agent
-from phi.model.huggingface import HuggingFaceChat
+from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
 
+load_dotenv()
 
-## web search agent
-
-web_search =Agent(
+## Web Search Agent
+web_search = Agent(
     name="web_search_agent",
     role="Search the web for information",
-    model =HuggingFaceChat(id="deepseek-ai/DeepSeek-V3"),
+    model=Groq(id="Llama3-8b-8192"),  # FIXED
     tools=[DuckDuckGo()],
     instructions=["Always include sources"],
     show_tool_calls=True,
     markdown=True
 )
 
-## Finance agent
-
-finance_agent =Agent(
+## Finance Agent
+finance_agent = Agent(
     name="Finance_agent",
-    role= ""
-    model = HuggingFaceChat(id="deepseek-ai/DeepSeek-V3")
+    model=Groq(id="Llama3-8b-8192"),  # FIXED
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                         company_news=True)]
+                         company_news=True)],
+    instructions=["Use table to display the data"],
+    show_tool_calls=True,
+    markdown=True
 )
 
+## Multi-Agent System
+multi_ai_agent = Agent(
+    team=[web_search, finance_agent],
+    model=Groq(id="Llama3-8b-8192"), # Replace OpenAI with Hugging Face
+    instructions=[
+        "Use the web search agent to find information about the company and the financial agent to find information about the stock.",
+        "Use a table to display the data."
+    ],
+    show_tool_calls=True,
+    markdown=True
+)
+
+
+multi_ai_agent.print_response("Summarize analyst recommendation and news for NVDA",stream=True)
